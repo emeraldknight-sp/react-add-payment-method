7f7fe972cbee791103b81a8d8b08c83ae3c9192e
David Almeida <contato.davidalmeida@outlook.com>
2022-11-10 19:56:40 -0300

    ✅ Add: Component Panel

---
+++ src/components/Panel/Panel.jsx
@@ -0,0 +1,5 @@
+import { StyledPanel } from "./Panel.style";
+
+export const Panel = ({ children }) => {
+  return <StyledPanel>{children}</StyledPanel>;
+};
---
+++ src/components/Panel/Panel.style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const StyledPanel = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  background-color: var(--gray-800);
+  width: 800px;
+  height: 500px;
+  padding: 28px 48px;
+  border: 1px solid var(--border);
+  border-radius: 6px;
+`;
